03976421ec1d4060fd6379a6507adb77075d5bad
snowlizard <illegal_burrito@live.com>
2022-01-16 22:53:00 -0800

    boot complete lol

--- src/components/bootloader.tsx
+++ src/components/bootloader.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import boot from '../assets/bootloader_pre.gif';
 import boot2nd  from '../assets/bootloader_2nd.mp4';
+import vista_logon_logo from '../assets/vista_logon_logo.png';
+import loadingCircle from '../assets/loading_circle.png';
 
 export const Bootloader = () => {
     const [bootOrder, setBoot] = useState('pre');
@@ -11,16 +13,29 @@ export const Bootloader = () => {
             setBoot('boot2nd');
         }, 4000);
 
-        return () => clearTimeout(timeout);
+        const timeout2 = setTimeout( () => {
+            clearTimeout(timeout);
+            setBoot('bootFinal');
+        }, 8000);
+
+        return () => clearTimeout(timeout2);
     }, []);
 
     return (
         <div className="bootloader_container">
             {
                 bootOrder === 'pre' ? <img id="bootloader_pre" src={boot}></img>
-                : <video id="bootloader_2nd" width={400} height={400} autoPlay muted>
+                : bootOrder === 'boot2nd' ? 
+                <video id="bootloader_2nd" width={400} height={400} autoPlay muted>
                     <source src={boot2nd} type="video/mp4"></source>
-                  </video>
+                </video>
+                : <div id="bootloader_final">
+                    <div className="bootWelcome">
+                        <img id="loadingCircle" src={loadingCircle}></img>
+                        <span id="boot_text" >Welcome</span>
+                    </div>
+                    <img id="bootlogo_bottom" src={vista_logon_logo}></img>
+                  </div>
             }
         </div>
     );
