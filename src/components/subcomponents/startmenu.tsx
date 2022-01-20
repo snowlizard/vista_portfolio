d3fb6294a7fbd115abc29c61f171fcc955907ee8
snowlizard <illegal_burrito@live.com>
2022-01-19 22:45:45 -0800

    added some stuff to the start menu

--- src/components/subcomponents/startmenu.tsx
+++ src/components/subcomponents/startmenu.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, Divider } from '@mui/material';
+import userFrame from '../../assets/ui/UserFrame.png';
+import userPicture from '../../assets/ui/user_picture.jpg';
+import searchIcon from '../../assets/ui/searchicon.png';
 
 export const StartMenu = () => {
     return (
@@ -9,9 +12,30 @@ export const StartMenu = () => {
                 <div className='searchBar'>
                     <input id='search__bar' type='text'
                     placeholder='Start Search'></input>
+                    <img id='searchIcon' src={searchIcon}></img>
                 </div>
             </div>
-            <div className='placelist'></div>
+            <div className='placelist'>
+                <div id='rightTop'>
+                    <img className='userFrame' src={userFrame}></img>
+                    <img id='userPic' src={userPicture}></img>
+                </div>
+
+                <span className='placelist_text' >Julian</span>
+                <span className="placelist_text">Documents</span>
+                <span className="placelist_text">Pictures</span>
+                <span className="placelist_text">Music</span>
+                <span className="placelist_text">Videos</span>
+                <Divider className='placelist_divider' variant='middle'/>
+                <span className="placelist_text">Games</span>
+                <span className="placelist_text">Computer</span>
+                <Divider className='placelist_divider' variant='middle'/>
+                <span className="placelist_text">Control Panal</span>
+                <span className="placelist_text">Devices and Printers</span>
+                <span className="placelist_text">Default Programs</span>
+                <span className="placelist_text">Help and Support</span>
+                
+            </div>
         </Box>
     );
 }
