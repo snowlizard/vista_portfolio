b7f00f67e4c5a1abc34da9b155cc17c12c8a0867
snowlizard <illegal_burrito@live.com>
2022-01-23 01:25:27 -0800

    added window buttons

--- src/components/subcomponents/window.tsx
+++ src/components/subcomponents/window.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import Draggable from 'react-draggable';
+import close from '../../assets/ui/close.png';
+import min from '../../assets/ui/minimize.png';
+import max from '../../assets/ui/maximize.png';
 
 type windowProps = {
     content?: React.ReactNode
@@ -14,6 +17,11 @@ export const Window: React.FC<windowProps> = ({content, title}) => {
             <div className='window'>
                 <div id='dragStrip'>
                     <div className='windowTitle' >{title}</div>
+                    <div id='windowButtons'>
+                        <img id='minIcon' src={min}></img>
+                        <img id='maxIcon' src={max}></img>
+                        <img id='closeIcon' src={close}></img>
+                    </div>
                 </div>
                 <div className='windowBackground'></div>
                 <div className='windowContents'>
