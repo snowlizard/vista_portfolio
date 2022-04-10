d7662f5b6431f3b25913826655a8bfb87408d066
snowlizard <illegal_burrito@live.com>
2022-04-09 22:43:13 -0700

    added volume slider component

--- src/components/desktop.tsx
+++ src/components/desktop.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useSelector, RootStateOrAny } from "react-redux";
 import { Taskbar } from "./subcomponents/taskbar";
 import { Icons } from "./subcomponents/desktopIcons";
+import { Slider } from "@mui/material";
 
 export const Desktop = () => {
     const apps = useSelector( (state: RootStateOrAny) => state.apps);
@@ -20,6 +21,12 @@ export const Desktop = () => {
                     if(app.open) return app.app
                 })
             }
+
+            <div className="volume_slider">
+                <Slider defaultValue={45} orientation="vertical"
+                    valueLabelDisplay="auto" 
+                    sx={{ color : '#fff', height: "165px" }} />
+            </div>
         </div>
     );
 }
