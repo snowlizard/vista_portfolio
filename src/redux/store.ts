11d8d9880e143be8a517065775822f1f858f859d
snowlizard <illegal_burrito@live.com>
2022-04-09 23:14:01 -0700

    wired up volume control

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,14 @@ import { updateBootState } from "./reducers/bootStateReducer";
 import { updateAppState } from "./reducers/appsReducer";
 import { updateStartmenuState } from "./reducers/startmenuReducer";
 import { changeWallpaper } from "./reducers/wallpaperReducer";
+import { volumeReducer } from "./reducers/volumeReducer";
 
 const reducers = combineReducers({
     boot: updateBootState,
     apps: updateAppState,
     menu: updateStartmenuState,
-    wallpaper: changeWallpaper
+    wallpaper: changeWallpaper,
+    volume   : volumeReducer
 });
 
 export const store = createStore(reducers);
