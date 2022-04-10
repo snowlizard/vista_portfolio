134d8a151f15fb16e6772a13d2c5f218ffd7dbe0
snowlizard <illegal_burrito@live.com>
2022-04-09 23:13:15 -0700

    addd volume control reducer redux

---
+++ src/redux/reducers/volumeReducer.ts
@@ -0,0 +1,13 @@
+interface actionObj {
+    type: string,
+    state: boolean
+}
+
+export const volumeReducer = (state: boolean = false, action: actionObj) => {
+    switch (action.type){
+        case 'VOLUME_CONTROL':
+            return !action.state
+        default:
+            return state
+    }
+}
